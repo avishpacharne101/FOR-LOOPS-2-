2bc3361207562b6852cd9819167dc0cde1d81df4
avishpacharne101 <avishpacharne101@gmail.com>
2024-08-23 23:10:35 +0530

    Add files via upload

---
+++ pro10.dart
@@ -0,0 +1,10 @@
+void main(){
+  for(int i=20;i<=70;i++){
+       if(i%2==0){
+        print("even no, cube of $i=${i*i*i}");
+       }else{
+         print("odd no, square of $i=${i*i}");
+
+       }
+  }
+}
